2ac0b655e23501861ec65948fe9666ce20d21480
U-ZORYA\namatv <mnasoft@gmail.com>
2017-02-28 16:38:43 +0200

    2017-2-28 16:38:39 KO11-118383

--- test.lisp
+++ test.lisp
@@ -17,26 +17,20 @@
     ((and (numberp a )) (format out "<td>~6,3E</td>" a))
     (T (format out "<td>~A</td>" a))))
 
-(defun list-list-html-table(l f_name)
-  "
-(list-list-html-table '((1 2 3)(2 3 4) (5 6 7)) #p\"D:/home/_namatv/tmp/123.html\") "
-  (with-open-file (out f_name :direction :output :if-exists :supersede)
-    (list-html l :out out)))
-
-(defun list-html (l &key (out t) (charset "utf8") (header nil))
-  (format out "<html>
-<head>
-<meta charset=~A\">
-</head>~%" charset)
-  (when header (format out "<h1>~A</h1>~%" header))
-  (format out "<table>~%")
-  (mapcar
-   #'(lambda(l1)
-       (format out "<tr>")
-       (mapcar #'(lambda(el) (type_tr el out)) l1)
-       (format out "</tr>~%"))
-   l)
-  (format out "</table>~%</html>~%"))
-
 ;;(list-html '((1 2 3)(2 3 4) (5 6 7)))
 ;;(list-list-html-table '((1 2 3)(2 3 4) (5 6 7)) #p"D:/home/_namatv/tmp/123.html")
+
+(type_tr 0.0000123456789)
+(type_tr 0.000123456789)
+(type_tr 0.00123456789)
+(type_tr 0.0123456789)
+(type_tr 0.123456789)
+(type_tr 1.23456789)
+(type_tr 12.3456789)
+(type_tr 123.456789)
+(type_tr 1234.56789)
+(type_tr 12345.6789)
+(type_tr 123456.789)
+(type_tr 1234567.89)
+(type_tr 12345678.9)
+
