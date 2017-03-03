d4c25e238fa3b58913d4d650867a3fa395601a3f
U-ZORYA\namatv <mnasoft@gmail.com>
2017-03-03 16:20:50 +0200

    2017-3-3 16:20:45 KO11-118383

--- html-table.lisp
+++ html-table.lisp
@@ -7,21 +7,22 @@
 (defun type_tr(a &optional (out nil))
   (cond
     ((and (numberp a ) (= a 0))      (format out "<td>~4,2F</td>" a))
-    ((and (numberp a ) (< a 0.01))   (format out "<td>~6,3E</td>" a))
-    ((and (numberp a ) (< a 0.1))    (format out "<td>~7,5F</td>" a))
-    ((and (numberp a ) (< a 1))      (format out "<td>~7,4F</td>" a))
-    ((and (numberp a ) (< a 10))     (format out "<td>~7,3F</td>" a))
-    ((and (numberp a ) (< a 100))    (format out "<td>~7,2F</td>" a))
-    ((and (numberp a ) (< a 1000))   (format out "<td>~7,1F</td>" a))
-    ((and (numberp a ) (< a 10000))  (format out "<td>~7,0F</td>" a))
-    ((and (numberp a ) (< a 100000)) (format out "<td>~7,0F</td>" a))
+    ((and (numberp a ) (< (abs a) 0.01))   (format out "<td>~6,3E</td>" a))
+    ((and (numberp a ) (< (abs a) 0.1))    (format out "<td>~7,5F</td>" a))
+    ((and (numberp a ) (< (abs a) 1))      (format out "<td>~7,4F</td>" a))
+    ((and (numberp a ) (< (abs a) 10))     (format out "<td>~7,3F</td>" a))
+    ((and (numberp a ) (< (abs a) 100))    (format out "<td>~7,2F</td>" a))
+    ((and (numberp a ) (< (abs a) 1000))   (format out "<td>~7,1F</td>" a))
+    ((and (numberp a ) (< (abs a) 10000))  (format out "<td>~7,0F</td>" a))
+    ((and (numberp a ) (< (abs a) 100000)) (format out "<td>~7,0F</td>" a))
     ((and (numberp a )) (format out "<td>~6,3E</td>" a))
     (T (format out "<td>~A</td>" a))))
 
 (defun list-list-html-table(l f_name)
-  "
-(list-list-html-table '((1 2 3)(2 3 4) (5 6 7)) #p\"D:/home/_namatv/tmp/123.html\") "
-  (with-open-file (out f_name :direction :output :if-exists :supersede)
+  "Пример использования:
+;;;; (list-list-html-table '((1 2 3)(2 3 4) (5 6 7)) #p\"D:/home/_namatv/tmp/123.html\")
+ "
+  (with-open-file (out f_name :direction :output :if-exists :supersede :external-format :utf8)
     (list-html l :out out)))
 
 (defun list-html (l &key (out t) (charset "utf-8") (header nil))
