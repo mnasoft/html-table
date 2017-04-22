4cc2fd7cb30b3b57aacd69df93a48329ca804e38
Nick Matvyeyev <mnasoft@gmail.com>
2017-04-22 16:59:53 +0300

    2017-4-22 16:59:52 MNASOFT-01

--- html-table.lisp
+++ html-table.lisp
@@ -4,9 +4,23 @@
 
 ;;; "html-table" goes here. Hacks and glory await!
 
+(defun round-val (a &optional (out nil))
+  (cond
+    ((and (numberp a ) (= a 0))            (format out "~A" (string-trim " " (format nil "~4,2F" a))))
+    ((and (numberp a ) (< (abs a) 0.01))   (format out "~A" (string-trim " " (format out "~6,3E" a))))
+    ((and (numberp a ) (< (abs a) 0.1))    (format out "~A" (string-trim " " (format out "~7,5F" a))))
+    ((and (numberp a ) (< (abs a) 1))      (format out "~A" (string-trim " " (format out "~7,4F" a))))
+    ((and (numberp a ) (< (abs a) 10))     (format out "~A" (string-trim " " (format out "~7,3F" a))))
+    ((and (numberp a ) (< (abs a) 100))    (format out "~A" (string-trim " " (format out "~7,2F" a))))
+    ((and (numberp a ) (< (abs a) 1000))   (format out "~A" (string-trim " " (format out "~7,1F" a))))
+    ((and (numberp a ) (< (abs a) 10000))  (format out "~A" (string-trim " " (format out "~7,0F" a))))
+    ((and (numberp a ) (< (abs a) 100000)) (format out "~A" (string-trim " " (format out "~7,0F" a))))
+    ((and (numberp a ))                    (format out "~A" (string-trim " " (format out "~6,3E" a))))
+    (T                                     (format out "~A"    a))))
+
 (defun type_tr(a &optional (out nil))
   (cond
-    ((and (numberp a ) (= a 0))      (format out "<td>~4,2F</td>" a))
+    ((and (numberp a ) (= a 0))            (format out "<td>~4,2F</td>" a))
     ((and (numberp a ) (< (abs a) 0.01))   (format out "<td>~6,3E</td>" a))
     ((and (numberp a ) (< (abs a) 0.1))    (format out "<td>~7,5F</td>" a))
     ((and (numberp a ) (< (abs a) 1))      (format out "<td>~7,4F</td>" a))
@@ -15,8 +29,8 @@
     ((and (numberp a ) (< (abs a) 1000))   (format out "<td>~7,1F</td>" a))
     ((and (numberp a ) (< (abs a) 10000))  (format out "<td>~7,0F</td>" a))
     ((and (numberp a ) (< (abs a) 100000)) (format out "<td>~7,0F</td>" a))
-    ((and (numberp a )) (format out "<td>~6,3E</td>" a))
-    (T (format out "<td>~A</td>" a))))
+    ((and (numberp a ))                    (format out "<td>~6,3E</td>" a))
+    (T                                     (format out "<td>~A</td>"    a))))
 
 (defun list-list-html-table(l f_name)
   "Пример использования:
--- package.lisp
+++ package.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:html-table
   (:use #:cl)
-  (:export list-list-html-table list-html)
+  (:export list-list-html-table list-html round-val)
   )
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
