305786a55b6dd2c30de611c6f2ae656ff83930f1
U-ZORYA\namatv <mnasoft@gmail.com>
2018-03-14 11:59:00 +0200

    2018-3-14 11:58:54 KO11-118383

--- html-table.lisp
+++ html-table.lisp
@@ -18,7 +18,7 @@
     ((and (numberp a ))                    (format out "~A" (string-trim " " (format nil "~6,3E" a))))
     (T                                     (format out "~A"    a))))
 
-(defun type_tr(a &optional (out nil))
+(defun type-tr-point (a &optional (out nil) )
   (cond
     ((and (numberp a ) (= a 0))            (format out "<td>~4,2F</td>" a))
     ((and (numberp a ) (< (abs a) 0.01))   (format out "<td>~6,3E</td>" a))
@@ -27,9 +27,25 @@
     ((and (numberp a ) (< (abs a) 10))     (format out "<td>~7,3F</td>" a))
     ((and (numberp a ) (< (abs a) 100))    (format out "<td>~7,2F</td>" a))
     ((and (numberp a ) (< (abs a) 1000))   (format out "<td>~7,1F</td>" a))
-    ((and (numberp a ) (< (abs a) 10000))  (format out "<td>~7,0F</td>" a))
-    ((and (numberp a ) (< (abs a) 100000)) (format out "<td>~7,0F</td>" a))
+    ((and (numberp a ) (< (abs a) 10000))  (format out "<td>~d</td>" (round a)))
+    ((and (numberp a ) (< (abs a) 100000)) (format out "<td>~d</td>" (round a)))
     ((and (numberp a ))                    (format out "<td>~6,3E</td>" a))
+    ((and (null    a ))                    (format out "<td></td>" ))
+    (T                                     (format out "<td>~A</td>"    a))))
+
+(defun type-tr-comma (a &optional (out nil) )
+  (cond
+    ((and (numberp a ) (= a 0))            (format out "~a" (substitute #\, #\. (format nil "<td>~4,2F</td>" a))))
+    ((and (numberp a ) (< (abs a) 0.01))   (format out "~a" (substitute #\, #\. (format nil "<td>~6,3E</td>" a))))
+    ((and (numberp a ) (< (abs a) 0.1))    (format out "~a" (substitute #\, #\. (format nil "<td>~7,5F</td>" a))))
+    ((and (numberp a ) (< (abs a) 1))      (format out "~a" (substitute #\, #\. (format nil "<td>~7,4F</td>" a))))
+    ((and (numberp a ) (< (abs a) 10))     (format out "~a" (substitute #\, #\. (format nil "<td>~7,3F</td>" a))))
+    ((and (numberp a ) (< (abs a) 100))    (format out "~a" (substitute #\, #\. (format nil "<td>~7,2F</td>" a))))
+    ((and (numberp a ) (< (abs a) 1000))   (format out "~a" (substitute #\, #\. (format nil "<td>~7,1F</td>" a))))
+    ((and (numberp a ) (< (abs a) 10000))  (format out "<td>~d</td>" (round a)))
+    ((and (numberp a ) (< (abs a) 100000)) (format out "<td>~d</td>" (round a)))
+    ((and (numberp a ))                    (format out "<td>~6,3E</td>" a))
+    ((and (null    a ))                    (format out "<td></td>" ))
     (T                                     (format out "<td>~A</td>"    a))))
 
 (defun list-list-html-table(l f_name)
@@ -39,7 +55,7 @@
   (with-open-file (out f_name :direction :output :if-exists :supersede :external-format :utf8)
     (list-html l :out out)))
 
-(defun list-html (l &key (out t) (charset "utf-8") (header nil))
+(defun list-html (l &key (out t) (charset "utf-8") (header nil) (point-to-comma t))
   (format out "<html>
 <head>
 <meta charset=\"~A\">
@@ -49,7 +65,11 @@
   (mapcar
    #'(lambda(l1)
        (format out "<tr>")
-       (mapcar #'(lambda(el) (type_tr el out)) l1)
+       (mapcar #'(lambda(el)
+		   (if point-to-comma
+		       (type-tr-comma el out)
+		       (type-tr-point el out)))
+	       l1)
        (format out "</tr>~%"))
    l)
   (format out "</table>~%</html>~%"))
